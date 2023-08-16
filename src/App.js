3d7a35b042677d0d18edae846e9bf9e79bb48631
vickypedia-12 <vikasmourya54321@gmail.com>
2023-08-16 13:17:29 +0530

    first commit backup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>HELLOW WORLD! Let's build the disney+ clone🚀 ! </h2>
+    </div>
+  );
+}
+
+export default App;
